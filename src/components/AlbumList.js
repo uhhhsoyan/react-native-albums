e1e78888570f6fa39ce7926c639b6a653512fd20
Eric Essoyan <eric.essoyan@gmail.com>
2019-02-06 22:21:09 -0800

    the card

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
     state = { albums: [] };
@@ -10,10 +11,16 @@ class AlbumList extends Component {
             .then(response => this.setState({ albums: response.data }));
     }
     
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
     render() {
         return (
             <View>
-                <Text>AlbumList!!!</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
