0d6a5b35548ca663e0cde666a49cb00176032fcf
Eric Essoyan <eric.essoyan@gmail.com>
2019-02-08 09:05:16 -0800

    card details

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,53 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+    const { 
+        thumbnailStyle,
+        headerTextStyle, 
+        headerContentStyle,
+        thumbnailContainerStyle 
+    } = styles;
+
     return (
-      <View>
-          <Text>{props.album.title}</Text>
-      </View>  
+      <Card>
+          <CardSection>
+              <View style={thumbnailContainerStyle}>
+                  <Image 
+                  style={thumbnailStyle}
+                  source={{ uri: thumbnail_image }} 
+                  />
+              </View>
+              <View style={headerContentStyle}>
+                <Text style={headerTextStyle}>{title}</Text>
+                <Text>{artist}</Text>
+              </View>
+          </CardSection>
+      </Card>  
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    }
 };
 
 export default AlbumDetail;
