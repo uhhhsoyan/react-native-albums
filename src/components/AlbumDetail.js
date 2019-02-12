60c75a3e60f9cd5561ec4a9cec4bb7c9012a82a6
Eric Essoyan <eric.essoyan@gmail.com>
2019-02-12 14:56:35 -0800

    touchables

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -3,14 +3,16 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image } = album;
+    const { title, artist, thumbnail_image, image } = album;
     const { 
         thumbnailStyle,
         headerTextStyle, 
         headerContentStyle,
-        thumbnailContainerStyle 
+        thumbnailContainerStyle,
+        imageStyle
     } = styles;
 
     return (
@@ -26,6 +28,17 @@ const AlbumDetail = ({ album }) => {
                 <Text style={headerTextStyle}>{title}</Text>
                 <Text>{artist}</Text>
               </View>
+          </CardSection>
+          
+          <CardSection>
+            <Image 
+            style={imageStyle}
+            source={{ uri: image }} 
+            />
+          </CardSection>
+
+          <CardSection>
+              <Button />
           </CardSection>
       </Card>  
     );
@@ -48,6 +61,11 @@ const styles = {
         alignItems: 'center',
         marginLeft: 10,
         marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,6 @@
 // Components AlbumList
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -20,9 +20,9 @@ class AlbumList extends Component {
 
     render() {
         return (
-            <View>
+            <ScrollView>
                 {this.renderAlbums()}
-            </View>
+            </ScrollView>
         );
     }
 }
